164a98ea2f47f73c0d4d232eea190a20dbbc6a1a
unknown <povsokny2021@gmail.com>
2026-01-01 05:31:29 -0800

    update navbar

--- components/home-page/navbar.tsx
+++ components/home-page/navbar.tsx
@@ -7,8 +7,10 @@ export function Navbar() {
     <nav className="fixed top-0 left-0 right-0 z-50 px-6 py-4 bg-white/50 backdrop-blur-[10px] shadow-sm shadow-gray-100b border-b border-gray-100">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <Image src="/logo.jpg" alt="Supersuvey logo" width={36} height={36} className="rounded-lg" />
-          <span className="font-bold text-xl text-brand-dark text-primary">Super Suvey</span>
+          <Link href="/" className="flex items-center gap-2" >
+            <Image src="/logo.jpg" alt="Supersuvey logo" width={36} height={36} className="rounded-lg" />
+            <span className="font-bold text-xl text-brand-dark text-primary">Super Suvey</span>
+            </Link>
         </div>
         <div className="hidden md:flex items-center gap-8">
           <Link href="#" className="text-sm font-medium hover:text-primary transition-colors">
@@ -21,9 +23,11 @@ export function Navbar() {
             About Us
           </Link>
         </div>
-        <Button variant="default" className=" px-6 py-3 h-auto text-sm">
-          Login
-        </Button>
+        <Link href="/login">
+          <Button variant="default" className=" px-6 py-3 h-auto text-sm">
+            Login
+          </Button>
+        </Link>
       </div>
     </nav>
   )
